cd5441b0b7eba1a46edfb35f1f6533ca5e817a07
Yelim Oh <yelim.oh1@gmail.com>
2017-08-01 20:27:48 +0000

    Rename generate_question method in questions class

--- questions.rb
+++ questions.rb
@@ -6,10 +6,10 @@ class Questions
         @num1 = Random.new.rand(1..20)
         @num2 = Random.new.rand(1..20)
         @answer = num1 + num2
-        @question = gen_question
+        @question = generate_question
     end
 
-    def gen_question
+    def generate_question
         question = "What does #{num1} plus #{num2} equal?"
     end
 end
